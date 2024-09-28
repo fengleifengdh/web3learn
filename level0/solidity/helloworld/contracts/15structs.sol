0d296168b18949c69e12472101a901f87b3b51fb
fengleifeng <fengleifengdh@gmail.com>
2024-09-28 11:31:32 +0800

    solidity

--- level0/solidity/helloworld/contracts/15structs.sol
+++ level0/solidity/helloworld/contracts/15structs.sol
@@ -1,52 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Array {
-    // Several ways to initialize an array
-    uint256[] public arr;//动态长度数组
-    uint256[] public arr2 = [1, 2, 3];//动态长度数组初始
-    // Fixed sized array, all elements initialize to 0 长度为10的数组
-    uint256[10] public myFixedSizeArr;
-
-    function get(uint256 i) public view returns (uint256) {
-        return arr[i];
+contract Error {
+    function testRequire(uint256 _i) public pure{
+        require(_i>10,"must >10");
     }
 
-    // Solidity can return the entire array.
-    // But this function should be avoided for
-    // arrays that can grow indefinitely in length.
-    //可以通过数组返回动态数组
-    function getArr() public view returns (uint256[] memory) {
-        return arr;
+        function testRevert(uint256 _i) public pure {
+        // Revert is useful when the condition to check is complex.
+        // This code does the exact same thing as the example above
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+ uint256 public num;
+
+    function testAssert() public view {
+        // Assert should only be used to test for internal errors,
+        // and to check invariants.
+
+        // Here we assert that num is always equal to 0
+        // since it is impossible to update the value of num
+        assert(num == 0);
     }
 
-    function push(uint256 i) public {
-        // Append to array
-        // This will increase the array length by 1.
-        arr.push(i);
-    }
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function pop() public {
-        // Remove last element from array
-        // This will decrease the array length by 1
-        arr.pop();
-    }
-
-    function getLength() public view returns (uint256) {
-        return arr.length;
-    }
-
-    function remove(uint256 index) public {
-        // Delete does not change the array length.
-        // It resets the value at index to it's default value,
-        // in this case 0
-        delete arr[index];
-    }
-
-    function examples() internal pure  {
-        // create array in memory, only fixed size can be created
-        uint256[] memory a = new uint256[](5);
-        a[0] = 1;
-        // uint[3] memory nftArr = [uint(1000), 1001, 1002]; 
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
     }
 }
