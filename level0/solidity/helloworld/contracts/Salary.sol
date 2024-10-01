ad5bd1d75c94d659c0e0f7a6b070bc280f3b6f4d
fengleifeng <540480452@qq.com>
2024-10-01 11:50:34 +0800

    solidity

--- level0/solidity/helloworld/contracts/Salary.sol
+++ level0/solidity/helloworld/contracts/Salary.sol
@@ -1,30 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-contract Event {
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-
-    event Deposit(address _from ,string _name,uint256 _value);
-    function deposit(string memory _name) public payable {
-        emit Deposit(msg.sender,_name,msg.value);
+contract MyNFT is ERC721 {
+    constructor() ERC721("MyNFT", "MNFT") {
     }
-    // address public owner;
-    // uint256 public account;
-    // constructor (){
-    //     owner = msg.sender;
-    //     account = 0;
-    // }
-    // // modifier onlyOwner(uint256 _account){
-    // //     require(msg.sender == owner,"only");
-    // // }
-
-    // modifier onlyOwner(uint256 _account){
-    //     require(msg.sender == owner,"only");
-    //     _;
-    // }
-    // function updateAccount(uint256 _account)public onlyOwner(_account){
-    //     account = _account  ;
-    // }
-
-
 }
+// contract Event {
+
+
+
+//     event Deposit(address _from ,string _name,uint256 _value);
+//     function deposit(string memory _name) public payable {
+//         emit Deposit(msg.sender,_name,msg.value);
+//     }
+//     // address public owner;
+//     // uint256 public account;
+//     // constructor (){
+//     //     owner = msg.sender;
+//     //     account = 0;
+//     // }
+//     // // modifier onlyOwner(uint256 _account){
+//     // //     require(msg.sender == owner,"only");
+//     // // }
+
+//     // modifier onlyOwner(uint256 _account){
+//     //     require(msg.sender == owner,"only");
+//     //     _;
+//     // }
+//     // function updateAccount(uint256 _account)public onlyOwner(_account){
+//     //     account = _account  ;
+//     // }
+
+
+// }
